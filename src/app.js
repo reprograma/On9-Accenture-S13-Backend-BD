a67d4fa232c04daed8f6b40f8d6543a8ebd30d20
Jennyffer Morais <jennyffernovaes@hotmail.com>
2020-11-07 21:20:33 -0300

    Criar projeto to do api

--- src/app.js
+++ src/app.js
@@ -1,24 +1,18 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/ToDoList', 
-{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/ToDoList", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-const index = require("./routes/index")
-const toDoRotas = require("./routes/toDoRoutes")
+const index = require("./routes/index");
+const toDoRotas = require("./routes/toDoRoutes");
 
+app.use(express.json());
 
-app.use(express.json())
+app.use("/", index);
+app.use("/tarefas", toDoRotas);
 
-app.use("/", index)
-app.use("/tarefas", toDoRotas)
-
-module.exports = app
-
-
-
-
+module.exports = app;
